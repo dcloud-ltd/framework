b04cf155997f2a0b05619fc47b2bf6f75c1fa4f5
吴培泽 <info@wupeize.com>
2024-10-22 19:08:47 +0800

    feat: framework package start

---
+++ src/Bootstrap.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace DCLOUD\Framework;
+class Bootstrap
+{
+    public function __construct()
+    {
+        echo __CLASS__.PHP_EOL;
+    }
+}
---
+++ tests/BootstrapTest.php
@@ -0,0 +1,7 @@
+<?php declare(strict_types=1);
+
+use DCLOUD\Framework\Bootstrap;
+
+include dirname(__DIR__) . '/vendor/autoload.php';
+
+new Bootstrap();
